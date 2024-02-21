82a298814ab8d2201f771aa2a8b39ce7cd5d9a3e
MikaelKhalil4 <128468052+MikaelKhalil4@users.noreply.github.com>
2024-02-21 19:53:58 +0200

    Putting the colors Global In Program.cs

--- MKproject/Program.cs
+++ MKproject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CustomizedTools;
 using System;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 
@@ -9,9 +10,15 @@ namespace MKproject
     internal static class Program
     {
         public static bool IsANewParentAddedOrParentPhoneUpdated;//this variable is used kermel lamma nerjaa aal search(nekbus back men el management), naamil restore men el datatbase 
-        //in 2 cases:1) Lamma naamil add la new parent men el new register,2) lamma naamil update la phone number tabaa parent eendo chiddrens
-      
-        
+                                                                 //in 2 cases:1) Lamma naamil add la new parent men el new register,2) lamma naamil update la phone number tabaa parent eendo chiddrens
+
+        //Global Colors:  Soft Gentle  Medium Vibrant Bold
+        public static Color SoftColor = Color.FromArgb(238, 241, 254);//used if the backgorund was white
+        public static Color MediumColor = Color.FromArgb(196, 210, 245);//used in the background
+        public static Color BoldColor = Color.FromArgb(109, 122, 224);//used for buttons/datagrid headers
+
+
+
         //Cashed Forms
         public static MKproject.Management.ClientManagementProfile clientManagementProfile;      
         public static MKproject.Management.NewRegister NewRegisterForm;
@@ -22,8 +29,10 @@ namespace MKproject
         public static GreyColor GreyForm;
         public static GreyColor GreyFormJunior;//in case eende 3 forms foe baaed metel bel new register
 
+        //Mikas: Data Source=MKpc;Initial Catalog=MKproject;User ID=sa;Password=1234
+        //Gabs:  Data Source=DESKTOP-MMI74FE\\SQLEXPRESS;Initial Catalog=MKproject2; Integrated Security=True
 
-        public static string DataLocation = "Data Source=DESKTOP-MMI74FE\\SQLEXPRESS;Initial Catalog=MKproject2; Integrated Security=True";
+        public static string DataLocation = "Data Source=MKpc;Initial Catalog=MKproject;User ID=sa;Password=1234";
         public static string FolderProfileImagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ProfileImages");
         public static string ExecptionString = "Unexpected error:\n";
 
