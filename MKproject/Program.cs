b3c73af8fc198ce11024dafdc6ac25bc4175c7a2
mikael khalil <mikaelkhalil7.mk@gmail.com>
2024-06-30 15:01:28 +0300

    pipeline testing

--- MKproject/Program.cs
+++ MKproject/Program.cs
@@ -172,8 +172,8 @@ namespace MKproject
 
 
         //         dotnet publish -c Release --self-contained -r win-x64 -o./bin/Publish/win-x64
-        //         vpk download http --url https://cdn.foxdigitaltech.online/fox-global 
-        //         vpk pack -u FoxApp -v 1.0.17 -p./bin/Publish/win-x64 -e MKproject.exe  --channel win-x64 --packTitle "Fox" --icon images/foxlogo.ico --splashImage images/foxlogo.ico 
+        //         vpk download http --url https://cdn.foxdigitaltech.online/fox-global
+        //         vpk pack -u FoxApp -v 1.0.20 -p./bin/Publish/win-x64 -e MKproject.exe  --channel win-x64 --packTitle "Fox" --icon images/foxlogo.ico --splashImage images/foxlogo.ico 
         //         vpk upload s3  --bucket fox-global  --channel win-x64 --endpoint http://198.7.119.42:9000  --keyId M7vOlSs7PznsJwiuGVyE --secret RwPBh65YQ3vi7mFNleszDzLCDe2aP3LSO4RS2Vdm
 
 
