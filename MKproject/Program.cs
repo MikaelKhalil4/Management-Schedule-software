5d80fe5ca79cb37daa40c4be8608dc4d5541ab3b
mikael khalil <mikaelkhalil7.mk@gmail.com>
2024-07-12 23:42:05 +0300

    some stuff

--- MKproject/Program.cs
+++ MKproject/Program.cs
@@ -206,16 +206,16 @@ namespace MKproject
             }
 
         }
-         
+
 
 
         //         dotnet publish -c Release --self-contained -r win-x64 -o./bin/Publish/win-x64
         //         vpk download http --channel win-x64 --url https://cdn.foxdigitaltech.online/fox-elk
-        //         vpk pack -u FoxApp -v 1.0.0 -p./bin/Publish/win-x64 -e MKproject.exe  --channel win-x64 --packTitle "Fox" --icon images/foxlogo.ico --splashImage images/foxlogo.ico 
-        //         vpk upload s3  --bucket fox-elk  --channel win-x64 --endpoint  http://198.7.119.42:9000 --keyId z7XrmE85WvdpJu66TZHs --secret LdalmmahMPdml5ChdACVYeebuoO1C7tVpQDFwMA4
-        //         https://cdn-admin.foxdigitaltech.online/fox-elk
+        //         vpk pack -u FoxApp -v 1.0.1 -p./bin/Publish/win-x64 -e MKproject.exe  --channel win-x64 --packTitle "Fox" --icon images/foxlogo.ico --splashImage images/foxlogo.ico 
+        //         vpk upload s3  --bucket fox-elk  --channel win-x64 --endpoint   https://cdn.foxdigitaltech.online/fox-elk --keyId z7XrmE85WvdpJu66TZHs --secret LdalmmahMPdml5ChdACVYeebuoO1C7tVpQDFwMA4
+        //         https://cdn.foxdigitaltech.online/fox-elk
 
-         
+
         //some global functions
         public static string SetCashFormat(string cash)
         {
