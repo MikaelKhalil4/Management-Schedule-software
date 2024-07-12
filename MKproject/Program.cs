b8cc5d97cfd0ac2169ca7bbcaae8017d1a1dd01c
mikael khalil <mikaelkhalil7.mk@gmail.com>
2024-07-12 20:27:54 +0300

    stuff

--- MKproject/Program.cs
+++ MKproject/Program.cs
@@ -130,7 +130,7 @@ namespace MKproject
                 else
                 {
                     LoginForm = new LOGIN();
-                    LoginForm.labelVersion.Text = "v 1.0.32";
+                    LoginForm.labelVersion.Text = "v 1.0.0";
                     Application.Run(LoginForm);
                 }
 
@@ -214,7 +214,7 @@ namespace MKproject
 
         //         dotnet publish -c Release --self-contained -r win-x64 -o./bin/Publish/win-x64
         //         vpk download http --channel win-x64 --url https://cdn.foxdigitaltech.online/fox-elk
-        //         vpk pack -u FoxApp -v 1.0.32 -p./bin/Publish/win-x64 -e MKproject.exe  --channel win-x64 --packTitle "Fox" --icon images/foxlogo.ico --splashImage images/foxlogo.ico 
+        //         vpk pack -u FoxApp -v 1.0.0 -p./bin/Publish/win-x64 -e MKproject.exe  --channel win-x64 --packTitle "Fox" --icon images/foxlogo.ico --splashImage images/foxlogo.ico 
         //         vpk upload s3  --bucket fox-elk  --channel win-x64 --endpoint  http://198.7.119.42:9000 --keyId z7XrmE85WvdpJu66TZHs --secret LdalmmahMPdml5ChdACVYeebuoO1C7tVpQDFwMA4
         //         https://cdn-admin.foxdigitaltech.online/fox-elk
 
